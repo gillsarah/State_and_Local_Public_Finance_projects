ff05dad6a7e09f6e004bb0da694a531471672cf7
Sarah Gill <gills@uchicago.edu>
2021-04-03 14:23:04 -0700

    Update plot_state_govt_finance.py
    playing with doing a tax source viz, issues with transpose not -> a df (it is supposed to I think). Going ahead and transposing in R

--- State-Finance-Comparison/build/plot_state_govt_finance.py
+++ State-Finance-Comparison/build/plot_state_govt_finance.py
@@ -5,12 +5,24 @@ import seaborn as sns
 import numpy as np
 
 os.chdir('/Users/Sarah/Documents/GitHub/State_and_Local_Public_Finance_projects/State-Finance-Comparison')
-df = pd.read_csv(os.path.join('data','far_west.csv'))
+df = pd.read_csv(os.path.join('data','far_west_t.csv'))
 
 df.columns
 
-CA_df = df.loc[[0,4]]
 
+CArev = df.iloc[0:1,2:15]
+revbysource = CArev[['Intergovernmental.revenue','Taxes','Current.charge', 
+                    'Miscellaneous.general.revenue', 'Utility.revenue','Liquor.stores.revenue']]
+taxrev = CArev[['Taxes', 'General.sales', 'Selective.sales', 'License.taxes',
+                'Individual.income.tax', 'Corporate.income.tax', 'Other.taxes']]
+taxrev_t = pd.DataFrame(np.transpose(taxrev))
+taxrev_t = pd.DataFrame(taxrev.transpose())
+
+
+
+fig, ax = plt.subplots()
+ax.bar(taxrev_t)
+plt.show()
 
 fig, ax = plt.subplots()
 ax.bar(df['Row.names'], df['General.expenditure'])
