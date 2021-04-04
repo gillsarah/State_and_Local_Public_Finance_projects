8b649aa6c995f4af8e8f6bdb43b7a38679c107a1
Sarah Gill <gills@uchicago.edu>
2021-04-03 21:11:05 -0700

    Update plot_state_govt_finance.py
    plotted taxes by source
    -> now I see that the revenue tables from the census are wrong (disagree with other census tables from the same year! missing Property tax (T01, but they say T01 is in Other, but other is way too low for how much property tax is reported in a different table for CA!)
    so need to fix that

--- State-Finance-Comparison/build/plot_state_govt_finance.py
+++ State-Finance-Comparison/build/plot_state_govt_finance.py
@@ -8,9 +8,36 @@ os.chdir('/Users/Sarah/Documents/GitHub/State_and_Local_Public_Finance_projects/
 df = pd.read_csv(os.path.join('data','far_west_t.csv'))
 
 df.columns
+df['Unnamed: 0']
+# clean up row title formatting
+df['Unnamed: 0'] = df['Unnamed: 0'].apply(lambda x: x.replace(".", " "))
+df['Unnamed: 0'] = df['Unnamed: 0'].apply(lambda x: x.title())
+
+# subset df
+taxbysource = df.iloc[5:11,]
+
+# sort by one state's values
+sort_CA = taxbysource.sort_values(by = 'California', ascending=True)
+
+# plot
+fig, ax = plt.subplots()
+ax.barh(sort_CA['Unnamed: 0'], sort_CA['California'])
+# label bars with values
+for i, v in enumerate(sort_CA['California']):
+    plt.text(v,i, '${:,.0f}'.format(round(v,0)) ) #0:.0f
+
+# Adjust layout to make room for the long lables:
+plt.subplots_adjust(left=0.3)
+ax.spines['right'].set_visible(False)
+ax.spines['left'].set_visible(False)
+ax.spines['top'].set_visible(False)
+ax.spines['bottom'].set_visible(False)
+plt.tick_params(bottom=False, labelbottom=False)
+plt.show()
 
 
-CArev = df.iloc[0:1,2:15]
+
+# out of date
 revbysource = CArev[['Intergovernmental.revenue','Taxes','Current.charge', 
                     'Miscellaneous.general.revenue', 'Utility.revenue','Liquor.stores.revenue']]
 taxrev = CArev[['Taxes', 'General.sales', 'Selective.sales', 'License.taxes',
