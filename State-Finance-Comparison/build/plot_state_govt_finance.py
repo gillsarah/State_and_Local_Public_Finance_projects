fa676bcd0b72a5cee88c8f1ef798a9511476cbdf
Sarah Gill <gills@uchicago.edu>
2021-04-04 13:22:30 -0700

    Update plot_state_govt_finance.py
    bar plot function, works (still sceptical about reprted property tax!)

--- State-Finance-Comparison/build/plot_state_govt_finance.py
+++ State-Finance-Comparison/build/plot_state_govt_finance.py
@@ -13,28 +13,43 @@ df['Unnamed: 0']
 df['Unnamed: 0'] = df['Unnamed: 0'].apply(lambda x: x.replace(".", " "))
 df['Unnamed: 0'] = df['Unnamed: 0'].apply(lambda x: x.title())
 
-# subset df
+# subset df for tax by source
 taxbysource = df.iloc[5:11,]
 
-# sort by one state's values
-sort_CA = taxbysource.sort_values(by = 'California', ascending=True)
+# subset df for revenue by source -not working yet
+revbysource = pd.concat(df.iloc[2:5,], df.iloc[6:15,])
+revbysource = revbysource.iloc[-5:11,]
 
-# plot
-fig, ax = plt.subplots()
-ax.barh(sort_CA['Unnamed: 0'], sort_CA['California'])
-# label bars with values
-for i, v in enumerate(sort_CA['California']):
-    plt.text(v,i, '${:,.0f}'.format(round(v,0)) ) #0:.0f
+def one_state_bar_plot(df, state, savefig = False):
+    '''df, already sliced 
+    state, string of state name (match column name for that state)
+    '''
+    # sort by one state's values
+    sort_state = df.sort_values(by = state, ascending=True)
 
-# Adjust layout to make room for the long lables:
-plt.subplots_adjust(left=0.3)
-ax.spines['right'].set_visible(False)
-ax.spines['left'].set_visible(False)
-ax.spines['top'].set_visible(False)
-ax.spines['bottom'].set_visible(False)
-plt.tick_params(bottom=False, labelbottom=False)
-plt.show()
+    # plot
+    fig, ax = plt.subplots()
+    ax.barh(sort_state['Unnamed: 0'], sort_state[state])
+    # label bars with values
+    for i, v in enumerate(sort_state[state]):
+        plt.text(v,i, '${:,.0f}'.format(round(v,0)) ) 
 
+    plt.title('Per Capita {} State Tax Revenue by source'.format(state))
+    # Adjust layout to make room for the long lables:
+    plt.subplots_adjust(left=0.3)
+    # clean up
+    ax.spines['right'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    ax.spines['bottom'].set_visible(False)
+    plt.tick_params(bottom=False, labelbottom=False)
+    if savefig:
+        plt.savefig('Per Capita {} State Tax Revenue by source'.format(state))
+    else:
+        plt.show()
+
+for v in df.columns[1:]:
+    one_state_bar_plot(taxbysource, v)
 
 
 # out of date
